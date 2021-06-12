311995d1681b0e08158c2662a28ce7441cd599a3
Steven Zych <stevezych@gmail.com>
2021-06-12 16:15:38 -0600

    added filtering methond and method to content to table once filtered

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,24 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+//Function to filter by input date
+function filterByDate(date) {
+    return date.datetime === intput;
+};
+
+// List of sightings to display
+var currentList = tableData.filter(filterByDate);
+
+// Use D3 to select table to input data
+var table = d3.select("#ufo-table");
+
+//Loop through each sighting using forEach and Object.entries
+tableData.forEach(function(ufo){
+    console.log(ufo);
+    table.append('tr');
+    Object.entries(ufo).forEach(function([key,value]){
+        var cells = table.appen('td');
+        cells.text(value);
+    });
+});
